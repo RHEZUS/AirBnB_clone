7523f097a801290b3fe802b4c8cb4d0c18ddaa19
Seunda <ludivinseunda2@gmail.com>
2024-02-05 13:12:01 +0300

    solve tasks

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Contains the BaseModel module
+"""
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+
+    def __init__(self):
+        """
+        Constructor method
+        """
+    
+    @property
+    def id(self):
+        """
+        Getter method
+        """
+        return self.id
+
+    @id.setter
+    def id(self):
+        """
+        Setter method
+        """
