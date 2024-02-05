317d4ca8d6a31d5cdf3f5d2fdadeec672f27892a
Seunda <ludivinseunda2@gmail.com>
2024-02-05 13:46:23 +0300

    solve tasks

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 import uuid
 from datetime import datetime
-from models import storage
 
 
 """
@@ -21,3 +19,14 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.update_at = datetime.now()
+    
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        diction = self.__dict__
+        diction ['__class__'] = self.__class__.__name__
+        return diction
