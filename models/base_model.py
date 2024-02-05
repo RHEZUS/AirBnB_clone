345e972a9022247a7cff74493d1baae44cdb4e94
Seunda <ludivinseunda2@gmail.com>
2024-02-05 14:55:49 +0300

    solve tasks

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,45 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
-
-
 """
 Contains the BaseModel module
 """
+import uuid
+from datetime import datetime
+
 
 class BaseModel:
     """
     BaseModel class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Constructor method
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+
+        if args:
+            self.id = args[0]
+            self.created_at = args[1]
+            self.updated_at = args[2]
+
+        elif not args and kwargs:
+            self.id = kwargs['id']
+            self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         diction = self.__dict__
+        diction['created_at'] = datetime.isoformat(diction['created_at'])
+        diction['updated_at'] = datetime.isoformat(diction['updated_at'])
         diction ['__class__'] = self.__class__.__name__
+
         return diction
