4064e6be17eda45cc328de1dc9d592a58e8bdbba
Seunda <ludivinseunda2@gmail.com>
2024-02-09 14:46:11 +0300

    solve tasks

--- models/base_model.py
+++ models/base_model.py
@@ -13,32 +13,38 @@ class BaseModel:
         """Constructor method"""
 
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     
     def __str__(self):
+        """Return the string representation of a class"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
+        """Saves a model in the storage"""
         self.updated_at = datetime.now()
         storage.save()
         
 
     def to_dict(self):
-        diction = self.__dict__.copy()
-        diction ['__class__'] = self.__class__.__name__
-        diction['created_at'] = datetime.isoformat(diction['created_at'])
-        diction['updated_at'] = datetime.isoformat(diction['updated_at'])
+        """Returns a dictionary representation of the class"""
+        diction = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                diction[key] = datetime.strftime(value, "%Y-%m-%dT%H:%M:%S.%f") if value else None
+            elif value is not None:
+                diction[key] = value
+
+        diction['__class__'] = self.__class__.__name__
         return diction
--- models/user.py
+++ models/user.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+import sys
+import os
+
+# Add the project directory to the Python path
+current_directory = os.path.dirname(os.path.abspath(__file__))
+project_directory = os.path.join(current_directory, "..")  # Adjust the path accordingly
+sys.path.append(project_directory)
+
 from models.base_model import BaseModel
 
 """
@@ -12,4 +20,6 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-    
+
+#user = User()
+#print(user.id)
