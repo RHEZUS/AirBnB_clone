4dad618429cf8ff742b27c20a5e25aaa04ef4d8f
Seunda <ludivinseunda2@gmail.com>
2024-02-11 02:11:37 +0300

    solve tasks

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -10,22 +10,22 @@ class TestAmenity(unittest.TestCase):
     def test_initialization_no_kwargs(self):
         review = Review()
         self.assertIsInstance(review, Review)
-        self.assertEqual(review.name, "")
+        self.assertEqual(review.place_id, "")
 
         review = Review()
-        review.name = "Amenity"
-        self.assertEqual(review.name, "Amenity")
+        review.place_id = "Amenity"
+        self.assertEqual(review.place_id, "Amenity")
 
     def test_attribute_value(self):
         review = Review()
 
-        review.name = "New name"
-        self.assertEqual(review.name, "New name")
+        review.place_id = "New name"
+        self.assertEqual(review.place_id, "New name")
 
     def test_string_representation(self):
         # Test the __str__ method
         review = Review()
-        review.name = "New Amen"
+        review.place_id = "New Amen"
         expected_str = "[Review] ({}) {}".format(review.id, review.__dict__)
         self.assertEqual(str(review), expected_str)
 
@@ -39,19 +39,19 @@ class TestAmenity(unittest.TestCase):
     def test_to_dict_method(self):
         # Test the to_dict method
         review = Review()
-        review.name = "New Amen"
+        review.place_id = "New Amen"
         review_dict = review.to_dict()
         self.assertIsInstance(review_dict, dict)
         self.assertIn('__class__', review_dict)
         self.assertIn('id', review_dict)
         self.assertIn('created_at', review_dict)
         self.assertIn('updated_at', review_dict)
-        self.assertEqual(review_dict['name'], "New Amen")
+        self.assertEqual(review_dict['place_id'], "New Amen")
 
     def test_storage_interaction(self):
         # Test interaction with storage
         review = Review()
-        review.name = "New Amen"
+        review.place_id = "New Amen"
         review.save()
         self.assertIn(
             f"{review.__class__.__name__}.{review.id}", storage.all())
